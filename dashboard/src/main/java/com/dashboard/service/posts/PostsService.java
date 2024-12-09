8c58789428897112d6e8b145e05ce41485bd6e51
mkong <rhdalsrn2@naver.com>
2024-12-09 15:07:59 +0900

    [ADD] 4장 마지막 까지, 게시글 수정/삭제 화면 만들기

--- dashboard/src/main/java/com/dashboard/service/posts/PostsService.java
+++ dashboard/src/main/java/com/dashboard/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.dashboard.service.posts;
 
 import com.dashboard.domain.posts.Posts;
 import com.dashboard.domain.posts.PostsRepository;
+import com.dashboard.web.dto.PostsListResponseDto;
 import com.dashboard.web.dto.PostsResponseDto;
 import com.dashboard.web.dto.PostsSaveRequestDto;
 import com.dashboard.web.dto.PostsUpdateRequestDto;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +30,20 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
--- dashboard/src/test/java/com/dashboard/web/PostsApiControllerTest.java
+++ dashboard/src/test/java/com/dashboard/web/PostsApiControllerTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PostsApiControllerTest {
+public class    PostsApiControllerTest {
 
     @LocalServerPort
     private int port;
